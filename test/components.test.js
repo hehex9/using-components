8201c3febff4eaae3601ac0accdedf45a9c5afc1
hehe <xpc1993@gmail.com>
2019-12-18 14:09:37 +0800

    refactor: rename globalPath to root

--- test/components.test.js
+++ test/components.test.js
@@ -3,22 +3,22 @@ const { findComponent } = require('../lib/components')
 
 const fixtureHome = fixture('pages/home/home.wxml')
 const fixtureProfile = fixture('pages/profile/profile.wxml')
-const globalPath = fixture()
+const root = fixture()
 
 describe('find components', () => {
   test('find child component', async () => {
     let r = await findComponent(fixtureHome, 'c-not-found')
     expect(r).toBe(null)
 
-    r = await findComponent(fixtureHome, 'c-two', { globalPath })
+    r = await findComponent(fixtureHome, 'c-two', { root })
     expect(r).toBe('components/c-two/c-two')
 
-    r = await findComponent(fixtureHome, 'c-three', { globalPath })
+    r = await findComponent(fixtureHome, 'c-three', { root })
     expect(r).toBe('/components/c-three/index')
   })
 
   test('find nearest component', async () => {
-    let r = await findComponent(fixtureProfile, 'c-two', { globalPath })
+    let r = await findComponent(fixtureProfile, 'c-two', { root })
     expect(r).toBe('/components/c-two/c-two')
   })
 })
@@ -27,19 +27,19 @@ describe('find components with options', () => {
   const file = fixture('pages/profile/profile.axml')
 
   test('find axml component', async () => {
-    let r = await findComponent(file, 'c-axml-one', { globalPath, ext: 'axml' })
+    let r = await findComponent(file, 'c-axml-one', { root, ext: 'axml' })
     expect(r).toBe('/components/c-axml-one/index')
   })
 
   test('find component from custom components', async () => {
     let r = await findComponent(file, 'cc-one', {
-      globalPath,
+      root,
       dirname: 'custom-components',
     })
     expect(r).toBe('/custom-components/cc-one/cc-one')
 
     r = await findComponent(file, 'cc-inner', {
-      globalPath,
+      root,
       dirname: 'custom-components',
     })
     expect(r).toBe('custom-components/cc-inner/index')
--- test/main.test.js
+++ test/main.test.js
@@ -2,10 +2,12 @@ const { fixture } = require('./util')
 const { usingComponents } = require('../lib/main')
 const { globalComponents } = require('../lib/components')
 
+const root = fixture()
+
 test('using components (c-one)', async () => {
   const path = fixture('components/c-one/c-one.wxml')
   const result = await usingComponents(path, {
-    globalPath: fixture(),
+    root,
     globalComponents: await globalComponents(path),
   })
 
@@ -15,7 +17,7 @@ test('using components (c-one)', async () => {
 test('using components (home)', async () => {
   const path = fixture('pages/home/home.wxml')
   const result = await usingComponents(path, {
-    globalPath: fixture(),
+    root,
     globalComponents: await globalComponents(path),
   })
 
@@ -28,7 +30,7 @@ test('using components (home)', async () => {
 test('using components (profile)', async () => {
   const path = fixture('pages/profile/profile.wxml')
   const result = await usingComponents(path, {
-    globalPath: fixture(),
+    root,
     globalComponents: await globalComponents(path),
   })
 
@@ -39,7 +41,7 @@ test('using components (profile.axml)', async () => {
   const path = fixture('pages/profile/profile.axml')
   const result = await usingComponents(path, {
     ext: 'axml',
-    globalPath: fixture(),
+    root,
     globalComponents: [],
   })
 
